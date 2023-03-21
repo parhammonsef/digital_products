9623c6d7149f29d5b2611227f766e84eaf2ea7e7
parham monsef <parhammonsef8@gmail.com>
2023-03-22 00:27:39 +0330

    product models added

--- products/models.py
+++ products/models.py
@@ -16,7 +16,38 @@ class Category(models.Model):
         verbose_name_plural = _('Categories')
 
 class Product(models.Model):
-     pass
+    title = models.CharField(_('title'), max_length=50)
+    description = models.TextField(_('description'), blank=True)
+    avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    categories = models.ManyToManyField('Category',verbose_name=_('categories'),blank=True)
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time '), auto_now=True)
+
+    class Meta:
+        db_table = 'products'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
+
 
 class File(models.Model):
-    pass
+    product = models.ForeignKey('Product',verbose_name=_('product'),on_delete=models.CASCADE)
+    title = models.CharField(_('title'), max_length=50)
+    is_enable = models.BooleanField(_('is enable'), default=True)
+    file = models.FileField(_('file'),upload_to='files/%y/%m/%d/ ')
+    created_time = models.DateTimeField(_('created time'), auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated time '), auto_now=True)
+
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('file')
+        verbose_name_plural = _('files')
+
+
+
+
+
+
+
+
+
